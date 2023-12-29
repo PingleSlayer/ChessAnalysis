d36d07a460e640f5a8c6d8be3fb8e75334db114d
PingleSlayer <43813295+PingleSlayer@users.noreply.github.com>
2023-12-29 22:52:41 +0100

    Update game_analysis.py

--- game_analysis.py
+++ game_analysis.py
@@ -6,7 +6,7 @@ from position_analysis import position_name, evaluation
 from move_analysis import move_score, move_info, move_tag
 
 
-def game_analysis(pgn_string):
+def game_analysis(pgn_string, engine_depth=None, engine_time=0.1):
     # Load the PGN
     game = chess.pgn.read_game(StringIO(pgn_string))
 
@@ -14,10 +14,10 @@ def game_analysis(pgn_string):
     for node in game.mainline():
         # Add position information to the game object
         position_name(node, write=True)
-        evaluation(node, write=True, engine_time=0.1)
+        evaluation(node, write=True, engine_depth=engine_depth, engine_time=engine_time)
 
         # Add move information to the game object
         move_info(node, write=True)
-        move_score(node, write=True, engine_time=0.1)
+        move_score(node, write=True, engine_depth=engine_depth, engine_time=engine_time)
 
     return game
