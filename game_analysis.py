6431d1daf4a98ba7f42157bedca0cdb9a7f7e8a5
PingleSlayer <lemahieuhenri9@gmail.com>
2023-12-30 16:59:20 +0100

    Update

--- game_analysis.py
+++ game_analysis.py
@@ -6,7 +6,45 @@ from position_analysis import position_comment, position_name, position_evaluati
 from move_analysis import move_score, move_info, move_tag
 
 
-def game_analysis(pgn_string, engine_depth=None, engine_time=0.1):
+def game_accuracy(game, write=False, engine_depth=None, engine_time=0.1):
+    total_white_move_score = 0
+    total_white_moves = 0
+    total_black_move_score = 0
+    total_black_moves = 0
+    # Iterate through the moves in the game to calculate average move score
+    for node in game.mainline():
+        if node.board().turn == chess.BLACK:
+            total_white_move_score += move_score(node, write=True, engine_depth=engine_depth, engine_time=engine_time)
+            total_white_moves += 1
+        else:
+            total_black_move_score += move_score(node, write=True, engine_depth=engine_depth, engine_time=engine_time)
+            total_black_moves += 1
+
+    avg_white_move_score = total_white_move_score / total_white_moves
+    avg_black_move_score = total_black_move_score / total_black_moves
+
+    total_white_deviation = 0
+    total_black_deviation = 0
+    # Iterate through the moves in the game to calculate standard deviation
+    for node in game.mainline():
+        if node.board().turn == chess.BLACK:
+            total_white_deviation += pow((move_score(node, write=True, engine_depth=engine_depth, engine_time=engine_time) - avg_white_move_score),2)
+        else:
+            total_black_deviation += pow((move_score(node, write=True, engine_depth=engine_depth, engine_time=engine_time) - avg_black_move_score),2)
+
+    stdev_white_move_score = total_white_deviation / total_white_moves
+    stdev_black_move_score = total_black_deviation / total_black_moves
+
+    # Delete from pgn        
+    if not write:
+        for node in game.mainline():
+            og_comment = position_comment(node)
+            node.comment = og_comment
+
+    return avg_white_move_score, stdev_white_move_score, avg_black_move_score, stdev_black_move_score
+
+
+def game_analysis(pgn_string, write=False, engine_depth=None, engine_time=0.1):
     # Load the PGN
     game = chess.pgn.read_game(StringIO(pgn_string))
 
@@ -22,5 +60,15 @@ def game_analysis(pgn_string, engine_depth=None, engine_time=0.1):
         move_score(node, write=True, engine_depth=engine_depth, engine_time=engine_time)
         move_tag(node, write=True, engine_depth=engine_depth, engine_time=engine_time)
 
-    return game
+    avg_white_move_score, stdev_white_move_score, avg_black_move_score, stdev_black_move_score = game_accuracy(game, write=True)
 
+    print(f"White -> avg move score: {avg_white_move_score}, stdev: {stdev_white_move_score}\n")
+    print(f"Black -> avg move score: {avg_black_move_score}, stdev: {stdev_black_move_score}\n")
+    
+    # Delete from pgn
+    if not write:
+        for node in game.mainline():
+            og_comment = position_comment(node)
+            node.comment = og_comment
+
+    return game
